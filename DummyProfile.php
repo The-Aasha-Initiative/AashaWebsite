12e3b08c3f36fcd224ac8beb72add8265e0d095e
Sachin Jadhav <jadhav.sachin1993@gmail.com>
2021-08-12 16:53:45 +0530

    Finished Dummy About page

---
+++ DummyProfile.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Align</title>
+    <link rel="shortcut icon" type="image/jpg" href="images/favicon.ico"/>
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;700&display=swap" rel="stylesheet"> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/gh/thelevicole/youtube-to-html5-loader@4.0.1/dist/YouTubeToHtml5.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/animations.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://allfont.net/allfont.css?fonts=league-spartan" rel="stylesheet" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merienda&display=swap" rel="stylesheet">
+</head>
+<body class="t-body">
+    <nav class="navbar navbar-expand-lg navbar-light nav-shadow prof-nav" style="background-color: #fff;">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbarToggler10"
+        aria-controls="myNavbarToggler10" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="myNavbarToggler10">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="DummyAbout.html">About</a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li> -->
+            </ul>
+            <a class="navbar-brand ms-auto" href="#"><img src="images/Jenna_Therapy_logo.png"></a>
+            <ul class="navbar-nav sm-icons mr-0 ms-auto">
+                <li class="nav-item"><a class="nav-link nav-social-link social-fb" href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li class="nav-item"><a class="nav-link nav-social-link social-in" href="#"><i class="fab fa-instagram"></i></a></li>
+                <li class="nav-item"><a class="nav-link nav-social-link social-tw" href="#"><i class="fab fa-twitter"></i></a></li>
+                
+            </ul>
+        </div>
+    </nav>
+
+    <div class="therapist-banner">
+        <!-- <img class="profile-banner-image" src="images/profile-banner1.jpg" alt="">  -->
+    </div>
+
+    <div class="therapist-container">
+
+        <div class="therapist-name-about bg-lighter animatedParent animateOnce" data-sequence="500" data-appear-top-offset='-269'>
+            <div class="therapist-photo mr-t light-bg animated fadeInLeftShort" data-id="1">
+                <img class="therapist-image" src="images/Jenna_Oberoi_Front_Photo.PNG" alt="">    
+            </div>
+
+            <div class="therapist-name light-bg">
+                <h1>Jenna Oberoi</h1>   
+            </div>
+
+            <div class="profile-about mr-t mr-b">
+                <div class="custom-content">
+                    
+                    <div class="about-me">
+                        <div class="custom-heading2 animated fadeInDownShort" data-id="2"> 
+                            <h2>About Me</h2>
+                        </div>    
+                        <p class="animated fadeInUpShort" data-id="2">I help you create a Serene Lifestyle, by rewiring your mind so you can fulfil your life’s purpose with a clear vision. It’s time to empower and change your old beliefs to new positive ones, so you can finally embrace your authentic self and live the abundant life you have always imagined.”
+                        </p>
+                        <p class="animated fadeInUpShort" data-id="2">Recognised by the British Psychological Society, and the International Council of Psychotherapist, as featured in Forbes, Belynder has helped many people, including Celebrities, Royalty and Athletes breakthrough negative thinking patterns.</p>
+
+                        <p class="animated fadeInUpShort" data-id="2">Belynder uses a combination of ground-breaking methodologies, including Psychotherapy and self-reflective therapy. She now specialises in overcoming Trauma, Child Therapy, Stress, Anxiety, Depression, Infertility, Weight control, Addictions and many other issues.</p>
+                    </div>
+                
+                </div>    
+            </div>
+
+        </div>
+
+        <div class="therapist-services bg-slighty-dark animatedParent animateOnce" data-sequence='400' data-appear-top-offset='-322'>
+            <!-- <div class="mhs-img">
+                 <img src="images/mhs.png" alt="" srcset=""> 
+            </div> -->
+            <div class="services">
+                <div class="custom-heading2 animated growIn" data-id='1'>
+                    <h2>Services I provide</h2>
+                </div>
+                <ul>
+                    <li class="animated fadeInLeft" data-id='2'>Psychotherapy</li>
+                    <li class="animated fadeInLeft" data-id='3'>Group Therapy</li>
+                    <li class="animated fadeInLeft" data-id='4'>Counselling</li>
+                </ul>
+            </div>
+            <div class="specialization">
+                <div class="custom-heading2 animated growIn" data-id='1'>
+                    <h2>I specialize in</h2>
+                </div>
+                <ul>
+                    <li class="animated fadeInRight" data-id='2'>Anxiety</li>
+                    <li class="animated fadeInRight" data-id='3'>Trauma</li>
+                    <li class="animated fadeInRight" data-id='4'>Depression</li>
+                    <li class="animated fadeInRight" data-id='4'>Addiction</li>
+                    <li class="animated fadeInRight" data-id='4'>Relationship Issues</li>
+                    <li class="animated fadeInRight" data-id='4'>Lonliness</li>
+                    <li class="animated fadeInRight" data-id='4'>Work Performace Issues</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="therapist-other bg-lighter animatedParent animateOnce" data-appear-top-offset='-362'>
+                <div class="animated growIn">
+                    <video id="yt-iframe" data-yt2html5="https://www.youtube.com/watch?v=5qeS5yrak_8" controls></video>
+                </div>
+        </div>
+
+        <div class="therapist-contact bg-slighty-dark animatedParent animateOnce" data-sequence='400' data-appear-top-offset='-272'>
+            <div class="contact-me animated bounceInLeft" data-id='1'>
+                <div class="custom-heading2">
+                    <h2>Contact Me</h2>
+                </div>
+                <p></p>
+                <h5 class="animated fadeInLeft" data-id='2'>Hours</h5>
+                <p class="animated fadeInRight" data-id='2'>Mon to Fri</p>
+                <p class="animated fadeInRight" data-id='2'>9:00 PM to 5:00 PM</p> 
+                <h5 class="animated fadeInLeft" data-id='2'>Phone</h5> 
+                <p class="animated fadeInRight" data-id='2'>9234259645</p>
+                <h5 class="animated fadeInLeft" data-id='2'>Email</h5> 
+                <p class="animated fadeInRight" data-id='2'>johndoe@gmail.com</p>
+            </div>
+
+            <div class="therapist-map animated bounceInRight" data-id='1'>    
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15555.809455620778!2d77.5963265!3d12.91078325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae152ac5e81903%3A0xd599c95166a87880!2sVEGA%20CITY%20MALL!5e0!3m2!1sen!2sin!4v1627650252401!5m2!1sen!2sin" width="550" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>  
+        </div>
+
+    </div>
+
+    <div class="footer-basic t-footer therapist-footer" >
+        <footer class="animatedParent animateOnce" data-appear-top-offset='-12'>
+            <div class="t-footer-text animated flip"><p class="copyright">© 2021 ALIGN</p></div>
+        </footer>
+    </div>
+    
+    <script>
+        var player = new YouTubeToHtml5({
+            autoload: false // Disable loading videos on init, `.load()` method is required.            
+        });
+
+        // Add loading class to video element
+        player.addAction('api.before', function(element) {
+            element.classList.add('is-loading');
+        });
+
+        // Remove loading class after API HTTP request completes.
+        player.addAction('api.after', function(element) {
+            // Wait for video to be playable before removing the loading class
+            element.addEventListener('loadeddata', function() {
+                element.classList.remove('is-loading');
+            });
+        });
+
+        // Now we can load videos.
+        player.load();
+    </script>
+
+    <script>
+        new YouTubeToHtml5({
+            withAudio: true // Filter streams to those with audio channels
+        });
+    </script>
+    <script type="text/javascript" src="js/css3-animate-it.js"></script>                        
+
+</body>
+</html> 
