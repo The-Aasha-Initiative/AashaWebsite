edcccf3863d5af761ae391e5b663adf97fa013fc
Sachin Jadhav <jadhav.sachin1993@gmail.com>
2021-08-30 21:36:41 +0530

    Fixed css issue in therapists.php page

--- align.sql
+++ align.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2021 at 07:01 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- Generation Time: Aug 30, 2021 at 05:57 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -34,7 +33,7 @@ CREATE TABLE `new_joinees` (
   `email` varchar(100) NOT NULL,
   `phone` varchar(12) NOT NULL,
   `linkedIn` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `new_joinees`
@@ -53,19 +52,21 @@ CREATE TABLE `personal details` (
   `Therapist ID` bigint(20) UNSIGNED NOT NULL,
   `Instagram Link` varchar(15) NOT NULL,
   `Linkedin Link` varchar(15) NOT NULL,
-  `Phone Number` int(10) NOT NULL,
+  `Phone Number` varchar(10) NOT NULL,
   `Aasha URL` varchar(15) NOT NULL,
   `Address` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `personal details`
 --
 
 INSERT INTO `personal details` (`Therapist ID`, `Instagram Link`, `Linkedin Link`, `Phone Number`, `Aasha URL`, `Address`) VALUES
-(1001, 'https://www.ins', 'https://www.lin', 2147483647, '#', 'S-69,Tamil Nadu'),
-(1002, 'https://www.ins', 'https://www.lin', 2147483647, '#', 'Gurgaon'),
-(1003, 'https://www.ins', 'https://www.lin', 2145814818, '#', 'S1-999, Amritsar');
+(1001, 'https://www.ins', 'https://www.lin', '2147483647', '#', 'JJ Colony, Madanpur Khadar, New Delhi, Delhi 11007'),
+(1002, 'https://www.ins', 'https://www.lin', '2147483647', '#', 'Gurgaon'),
+(1003, 'https://www.ins', 'https://www.lin', '2145814818', '#', 'Raj Nagar, Madanpur Khadar New Delhi, Delhi 110076'),
+(1004, 'https://www.ins', 'https://www.lin', '9742467865', '', ' 86, Block A, Sector 105, Noida, Uttar Pradesh 201'),
+(1005, 'https://www.ins', 'https://www.lin', '5267895477', '', 'Sikkarayapuram');
 
 -- --------------------------------------------------------
 
@@ -75,25 +76,27 @@ INSERT INTO `personal details` (`Therapist ID`, `Instagram Link`, `Linkedin Link
 
 CREATE TABLE `therapists` (
   `Therapist ID` bigint(20) UNSIGNED NOT NULL,
-  `Name` char(50) COLLATE utf8_unicode_ci NOT NULL,
-  `Designation` char(15) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Identifies As` char(25) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Client Group` char(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Languages` char(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Issues Treated` char(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Location` char(20) COLLATE utf8_unicode_ci NOT NULL,
-  `Intro` varchar(225) COLLATE utf8_unicode_ci NOT NULL,
-  `Image` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `Name` char(50) NOT NULL,
+  `Designation` char(15) NOT NULL,
+  `Identifies As` char(25) NOT NULL,
+  `Client Group` char(100) NOT NULL,
+  `Languages` char(100) NOT NULL,
+  `Issues Treated` char(100) NOT NULL,
+  `Location` char(20) NOT NULL,
+  `Intro` varchar(225) NOT NULL,
+  `Image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `therapists`
 --
 
 INSERT INTO `therapists` (`Therapist ID`, `Name`, `Designation`, `Identifies As`, `Client Group`, `Languages`, `Issues Treated`, `Location`, `Intro`, `Image`) VALUES
-(1001, 'Sreerag', 'Aasha CEO', 'Falcon', 'Kids', 'English', 'Mental Health', 'Delhi', 'Hello guys, I am a stand-up comic by profession and stream counter strike too.', 'images/sreerag.jpg'),
-(1002, 'Kanishk', 'Web Developer', 'Fogripper', 'Kids', 'English', 'Website', 'Delhi', '', 'images/kanishk.jpg\r\n'),
-(1003, 'Ishdeep', 'Doctor', 'Sasha', 'Children', 'English', 'Chronic Pain', 'Delhi', 'CS will always remain special for me. Busy ranking up IRL.', 'images/ishdeep.jpg');
+(1001, 'Sreerag', 'Counsellor', 'Asian', 'Children', 'English', 'Anxiety', 'Delhi', 'Hello guys, I am a stand-up comic by profession and stream counter strike too.', 'images/therapists/sreerag.jpg'),
+(1002, 'Kanishk', 'Counsellor', 'Fogripper', 'Adults', 'English', 'Eating', 'Delhi', '', 'images/therapists/kanishk.jpg\r\n'),
+(1003, 'Ishdeep', 'Psychologist', 'Sasha', 'Adults', 'English', 'Depression', 'Delhi', 'CS will always remain special for me. Busy ranking up IRL.', 'images/therapists//ishdeep.jpg'),
+(1004, 'Memento', 'Psychologist', 'Asian', 'Children', 'English, Kannada', 'Trauma', 'Delhi', 'Competent, compassionate, and uses an eclectic approach with affordable charges.', 'images/therapists/memento.jpg'),
+(1005, 'Chatur', 'Psychologist', 'Asian', 'Couples', 'Tamil\r\n', 'Relationship', 'Chennai', '', '');
 
 -- --------------------------------------------------------
 
@@ -108,7 +111,7 @@ CREATE TABLE `therapists_contact_details` (
   `email` varchar(120) NOT NULL,
   `phone` varchar(12) NOT NULL,
   `instagramHandle` varchar(120) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `therapists_contact_details`
@@ -116,6 +119,7 @@ CREATE TABLE `therapists_contact_details` (
 
 INSERT INTO `therapists_contact_details` (`name`, `professional_title`, `qualifications`, `email`, `phone`, `instagramHandle`) VALUES
 ('Kanishk', 'Counsellor', 'Btech', '0', '2147483647', 'https://www.instagram.com/kanishk.grover/'),
+('Sachin Jadhav', 'Psychologist', 'MBBS', 'jj@gmail.com', '8974247069', 'https://www.instagram.com/222/'),
 ('Memento', 'Psychiatrist', 'MBBS', 'sachinj28939@gmail.com', '975506345', '@Memento_mori22');
 
 --
@@ -157,13 +161,13 @@ ALTER TABLE `therapists_contact_details`
 -- AUTO_INCREMENT for table `personal details`
 --
 ALTER TABLE `personal details`
-  MODIFY `Therapist ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1004;
+  MODIFY `Therapist ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1008;
 
 --
 -- AUTO_INCREMENT for table `therapists`
 --
 ALTER TABLE `therapists`
-  MODIFY `Therapist ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1004;
+  MODIFY `Therapist ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
 
 --
 -- Constraints for dumped tables
